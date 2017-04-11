5088150a161a00d93f16f0970a639585ecba23ba
unknown <antonlubushko@gmail.com>
2017-04-11 20:25:32 +0300

    this is first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import 'babel-polyfill'
+import React from 'react'
+import { Router, browserHistory } from 'react-router'
+import { routes } from './routes'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import {render} from 'react-dom'
+
+function playlists(state = [], action){
+    if(action.type === 'ADD_PLAYLIST'){
+        return state;
+    }else if (action.type === 'DELETE_PLAYLIST') {
+        return state;
+     }
+    return state;
+}
+
+const store = createStore(playlists,applyMiddleware(thunk));
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('root')
+);
